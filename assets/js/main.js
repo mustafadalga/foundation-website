f1cdbc84fd7df7ea4fe7aafd30bef0ca2210dec8
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-11-11 22:41:20 +0300

    testimonials section is completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,45 @@ features.forEach(feature=>{
         event.currentTarget.classList.toggle('active')
     })
 })
+
+
+
+/* Testimonials */
+
+var testimonials=document.querySelectorAll('.testimonial');
+var testimonials_dot=document.querySelectorAll('.testimonial-dot');
+var testimonial_is_selected=false;
+var index=0;
+
+testimonials_dot.forEach((testimonial,index)=>{
+    testimonial.addEventListener('click',(event)=>{
+        testimonial_is_selected=true
+        resetTestimonialActive()
+        changeSlide(index)
+        event.currentTarget.classList.add('active');
+    });
+})
+
+function resetTestimonialActive(){
+    testimonials_dot.forEach(item=>{
+        item.classList.remove("active");
+    })
+}
+function changeTestimonial(){
+    resetTestimonialActive();
+    changeSlide()
+    testimonials_dot[index].classList.add('active');
+}
+function changeSlide(indexNo=null){
+    testimonials[0].style.marginLeft=`-${indexNo!==null ? indexNo : index}00%`;
+}
+function durationSlider(){
+    setInterval(()=>{
+        if (!testimonial_is_selected){
+            changeTestimonial()
+            index<testimonials.length-1 ? index++ : index=0;
+        }
+    },3000);
+}
+
+durationSlider();

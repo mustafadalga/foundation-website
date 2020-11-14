ad55aae31ca0b255cf4420df62457d3945caa0df
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-11-14 21:22:37 +0300

    header responsive design completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,3 +83,21 @@ tabMenu.forEach(tabItem=>{
    });
 });
 
+
+/* Responsive Natigation Menu */
+
+var header = document.querySelector('header');
+var navMenu = header.querySelector('.nav-header-menu');
+var hamburberMenu = header.querySelector('.hamburger-icon')
+var menuLibraryItem=navMenu.querySelector('.nav-library-item');
+
+
+hamburberMenu.addEventListener('click', (event) => {
+    header.classList.toggle('open');
+});
+
+menuLibraryItem.addEventListener('click',(event)=>{
+    event.currentTarget.classList.toggle('active');
+})
+
+
